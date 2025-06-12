90aeca30f6962cf1ca39cf69ebdcda1a2f1fa15c
Ronald Almeida <ronaldmateus785@gmail.com>
2025-06-11 23:29:53 -0300

    feat: add ThemeProvider with system default theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import { Toaster } from "sonner";
 import FileExplorer from "./components/FileExplorer";
 import SidebarComponent from "./components/sidebar/Sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { ThemeProvider } from "@/components/theme-provider";
 
 function App() {
     return (
-        <SidebarProvider>
-            <div className="flex w-full">
-                <SidebarComponent />
-                <FileExplorer />
-                <Toaster />
-            </div>
-        </SidebarProvider>
+        <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+            <SidebarProvider>
+                <div className="flex w-full">
+                    <SidebarComponent />
+                    <FileExplorer />
+                    <Toaster />
+                </div>
+            </SidebarProvider>
+        </ThemeProvider>
     );
 }
 
